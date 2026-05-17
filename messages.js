160bc0eeaeef8b5f05e9318da037b2c0b9a012f5
bushmann24 <javonapi24@gmail.com>
2026-05-17 13:33:13 +1000

    Create messages.js

---
+++ messages.js
@@ -0,0 +1,26 @@
+// messages.js
+// You can add as many arrays in here as you want! 
+// Each array is a sequence of messages the restaurant will send.
+
+const restaurantPickupLines = [
+    [
+        "Hey 😏", 
+        "Want to see whats in between my buns?"
+    ],
+    [
+        "Are you hungry?", 
+        "Because I'm hot, ready, and 5 minutes away 🍕"
+    ],
+    [
+        "I heard you were looking for a snack.", 
+        "But I'm the whole meal. 🍔"
+    ],
+    [
+        "You must be starving.", 
+        "Let me satisfy that craving. 🌮"
+    ],
+    [
+        "I like my dates like I like my pizza...", 
+        "Extra cheesy and at your door in 30 minutes or less. 😉"
+    ]
+];
